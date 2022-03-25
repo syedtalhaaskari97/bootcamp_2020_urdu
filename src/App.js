7b1748a035a03e525437cba2967e87438edcce18
syedtalhaaskari <syedtalhaaskari@gmail.com>
2022-03-25 18:41:16 +0500

    Create boiler plate for upcoming bootcamp projects

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import Dinner from "./dinner";
-
 const App = () => {
   return (
     <div className="App">
       <h1>Session 3</h1>
-      <hr />
-      <Dinner dishName="Nihaari" sweetDish="Kheer" />
-      <hr />
-      <Dinner dishName="Biryani" sweetDish="Jaleebi" />
-      <hr />
-      <Dinner dishName="Karahi" sweetDish="Gajar ka Halwa" />
-      <hr />
     </div>
   )
 }
