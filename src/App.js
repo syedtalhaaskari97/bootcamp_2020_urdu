c37b281b5f8f7a853f0210d12e1c0c5f92d41be0
syedtalhaaskari <syedtalhaaskari@gmail.com>
2022-04-08 12:41:35 +0500

    Login Page Using Material UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { Button, TextField } from '@mui/material';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import "./App.css";
+import Navbar from './Components/Navbar';
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Session 3</h1>
-    </div>
+    <>
+      <Navbar />
+      <Paper elevation={7} style={{
+        width: 400,
+        margin: '0 auto',
+        textAlign: "center",
+        padding: 15,
+      }}>
+        <h1>Login</h1>
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': {
+              m: 1,
+              width: '95%',
+            },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField id="email-input" label="Enter Email" variant="standard" />
+          <TextField id="password-input" label="Enter Password" variant="standard" />
+          <Button variant="contained">Login</Button>
+        </Box>
+      </Paper>
+    </>
   )
 }
 
