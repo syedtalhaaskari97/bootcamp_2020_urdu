a97f5af40afec348842c2e9d6c69cdad3009240d
syedtalhaaskari <syedtalhaaskari@gmail.com>
2022-04-15 18:15:30 +0500

    52 30

--- src/Components/InfoPanel/index.js
+++ src/Components/InfoPanel/index.js
@@ -11,21 +11,45 @@ const Item = styled(Paper)(({ theme }) => ({
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
+    textTransform: "uppercase"
 }));
 
 export default function InfoPanel() {
+
+    const [globalData, setGlobalData] = React.useState(() => { })
+
+    React.useEffect(() => {
+        async function getData() {
+            const response = await fetch("https://covid19.mathdro.id/api")
+
+            let { confirmed, recovered, deaths, } = await response.json()
+            setGlobalData(() => ({
+                confirmed,
+                recovered,
+                deaths,
+            }))
+        }
+        getData()
+    }, [])
+
+    if (!globalData) return <>Loading...</>
+
     return (
         <Box className={styles.root} sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
-                <Grid item xs={12} md={4}>
-                    <Item elevation={3}>Grid 1</Item>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Item elevation={3}>Grid 2</Item>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Item elevation={3}>Grid 3</Item>
-                </Grid>
+                {
+                    Object.keys(globalData).map((key, ind) => {
+                        console.log(key);
+                        return (
+                            <Grid item xs={12} md={4} key={ind}>
+                                <Item elevation={3}>
+                                    <h3 className={styles.title}>{key}</h3>
+                                    <h3>{globalData[key].value}</h3>
+                                </Item>
+                            </Grid>
+                        )
+                    })
+                }
             </Grid>
         </Box>
     );
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -50,6 +50,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 export default function Navbar() {
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
